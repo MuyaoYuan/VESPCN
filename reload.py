a46b36bbacefe2587e9f40dc90edd9a6e6d5da85
yuanmuyao <yuanmuyao.stu@gmail.com>
2021-12-29 17:32:46 +0800

    微调

--- reload.py
+++ reload.py
@@ -1,40 +1,40 @@
 import numpy as np
 import torch
 import matplotlib.pyplot as plt
+from PIL import Image
 from torchsummary import summary
 from torch.utils.data import Dataset, DataLoader
 from dataset import *
 
-path_in = "dataset/walk_downscale"
-path_label = "dataset/walk"
-path_model = "trained_model/model_demo.pkl"
+def pictureProcess(picture_tensor):
+    picture_array = picture_tensor.detach().numpy()
+    picture_array = picture_array.transpose((0,2,3,1))
+    im_list = []
+    for picture_item in picture_array:
+        im_list.append(Image.fromarray(np.uint8(picture_item*255)))
+    return im_list
 
-net = torch.load(path_model)
-# print(summary(net,(3, 240, 360)))
+if __name__ =="__main__":
+    path_in = "dataset/walk_downscale"
+    path_label = "dataset/walk"
+    path_model = "trained_model/model_demo.pkl"
 
-myDataset = MyDataset(dir_input=path_in,dir_label=path_label,transform=ToTensor())
-myDataLoader = DataLoader(myDataset, batch_size=5, shuffle=True)
+    net = torch.load(path_model)
+    # print(summary(net,(3, 240, 360)))
 
-for i_batch, data_batch in enumerate(myDataLoader):
-    inputs = data_batch[0]
-    labels = data_batch[1]
-    outputs = net(inputs)
-    if i_batch == 0:
-        break
+    myDataset = MyDataset(dir_input=path_in,dir_label=path_label,transform=ToTensor())
+    myDataLoader = DataLoader(myDataset, batch_size=5, shuffle=True)
 
-plt.subplot(1,3,1)
-plt.imshow(inputs[1].detach().numpy().T)
-# print(np.max(inputs[1].detach().numpy()))
-# print(np.min(inputs[1].detach().numpy()))
-
-plt.subplot(1,3,2)
-plt.imshow(labels[1].detach().numpy().T)
-# print(np.max(labels[1].detach().numpy()))
-# print(np.min(labels[1].detach().numpy()))
-
-plt.subplot(1,3,3)
-plt.imshow(outputs[1].detach().numpy().T)
-# print(outputs[1].detach().numpy().T.shape)
-# print(np.max(outputs[1].detach().numpy()))
-# print(np.min(outputs[1].detach().numpy()))
-plt.show()
+    for i_batch, data_batch in enumerate(myDataLoader):
+        inputs = data_batch[0]
+        labels = data_batch[1]
+        outputs = net(inputs)
+        if i_batch == 0:
+            break
+    
+    inputs_im = pictureProcess(inputs)
+    labels_im = pictureProcess(labels)
+    outputs_im = pictureProcess(outputs)
+    inputs_im[1].save('result/input.png')
+    labels_im[1].save('result/label.png')
+    outputs_im[1].save('result/output.png')
--- trainer.py
+++ trainer.py
@@ -3,48 +3,52 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
-from torchsummary import summary
 from torch.utils.data import Dataset, DataLoader
 
 from model import *
 from dataset import *
 
-# 网络模型
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-net = ESPCN(n_colors=3, scale=2)
-net.to(device)
+if __name__ =="__main__":
+    # 网络模型
+    device = torch.device("cuda:1" if torch.cuda.is_available() else "cpu")
+    net = ESPCN(n_colors=3, scale=2)
+    net.to(device)
 
-# 数据集
-path_in = "dataset/walk_downscale"
-path_label = "dataset/walk"
-myDataset = MyDataset(dir_input=path_in,dir_label=path_label,transform=ToTensor())
-myDataLoader = DataLoader(myDataset, batch_size=5, shuffle=True)
+    # 数据集
+    path_in = "dataset/walk_downscale"
+    path_label = "dataset/walk"
+    myDataset = MyDataset(dir_input=path_in,dir_label=path_label,transform=ToTensor())
+    myDataLoader = DataLoader(myDataset, batch_size=5, shuffle=True)
 
-criterion = nn.MSELoss()
-optimizer = optim.Adam(net.parameters(), lr=0.001)
+    # criterion = nn.MSELoss()
+    criterion = F.binary_cross_entropy
+    optimizer = optim.Adam(net.parameters(), lr=0.001)
 
-init_weights(net)
+    init_weights(net)
 
-running_loss = 0.0
-for epoch in range(5):
-    for i_batch, data_batch in enumerate(myDataLoader):
-        # 数据
-        inputs = data_batch[0]
-        labels = data_batch[1]
-        # 一次梯度下降
-        optimizer.zero_grad()
-        outputs = net(inputs)
-        loss = criterion(outputs, labels)
-        loss.backward()
-        optimizer.step()
-        # train_loss
-        running_loss = loss.item()
-        print('[%d, %5d] loss:%.3f' %
-                  (epoch + 1, i_batch + 1, running_loss))
-print("Finished Training")
-torch.save(net, 'trained_model/model_demo.pkl')
-        
+    running_loss = 0.0
+    epochs = 20
+    for epoch in range(epochs):
+        for i_batch, data_batch in enumerate(myDataLoader):
+            # 数据
+            inputs = data_batch[0]
+            inputs = inputs.to(device)
+            labels = data_batch[1]
+            labels = labels.to(device)
+            # 一次梯度下降
+            optimizer.zero_grad()
+            outputs = net(inputs)
+            loss = criterion(outputs, labels)
+            loss.backward()
+            optimizer.step()
+            # train_loss
+            running_loss = loss.item()
+            print('[%d, %5d] loss:%.3f' %
+                    (epoch + 1, i_batch + 1, running_loss))
+    print("Finished Training")
+    torch.save(net, 'trained_model/model_demo.pkl')
+            
 
-        
+            
 
 
