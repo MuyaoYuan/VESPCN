9f3bda4b9acaa836c321859b046f3e1223f6c1a5
yuanmuyao <yuanmuyao.stu@gmail.com>
2022-06-09 09:42:44 +0800

    debug MA_loss

--- MA_loss.py
+++ MA_loss.py
@@ -3,10 +3,16 @@ import matplotlib.pyplot as plt
 
 def lossShow_epoch(title, xlabel, ylabel, color, curve_label, savepath, loss_arr, trainset = 800, batch_size = 10, epoch_limit = 200, MA = False):
     divide_span = int(trainset/batch_size/10)
-    print(len(loss_arr))
     epochs = min(int(len(loss_arr)/divide_span), epoch_limit)
     epochs_arr = np.arange(epochs) + 1
-    loss_arr_epoch = [loss_arr[(epoch) * divide_span - 1] for epoch in epochs_arr]
+    # loss_arr_epoch = [loss_arr[(epoch) * divide_span - 1] for epoch in epochs_arr]
+    loss_arr_epoch = []
+    for epoch in range(epochs):
+        loss = 0
+        for i in range(divide_span):
+            loss += loss_arr[(epoch) * divide_span + i]
+        loss /= divide_span
+        loss_arr_epoch.append(loss)
     if MA:
         pass
     plt.figure()
@@ -20,5 +26,5 @@ def lossShow_epoch(title, xlabel, ylabel, color, curve_label, savepath, loss_arr
 if __name__ == '__main__':
     lossShow_epoch("Learning Curve", "Epochs", "Loss value", "b", "Train Loss", 
                             '1channels.png',
-                            'trained_model/ESPCN/ESPCN_train_loss_arr.npy')
+                            np.load('trained_model/ESPCN/ESPCN_train_loss_arr.npy'), epoch_limit=200)
 
